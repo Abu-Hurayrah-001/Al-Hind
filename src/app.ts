bd0a456defb8025e34e36cc7a688d90c966f94fb
dedic <sahilakhtar5059@gmail.com>
2025-02-24 19:28:29 +0530

    Error Handling - part (c)

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 // IMPORTS.
 import express, { Application } from "express";
+import { errorHandler } from "./middlewares/errorHandlers/errorHandler.middleware";
 
 // APP.
-export const app: Application = express();
+const app: Application = express();
+app.use(errorHandler); // Always use it in the bottom of your code brother.
+export { app };
 
--- src/middlewares/errorHandlers/asyncErrorHandler.middleware.ts
+++ src/middlewares/errorHandlers/asyncErrorHandler.middleware.ts
@@ -2,3 +2,19 @@
 import { Request, Response, NextFunction } from "express";
 
 // ASYNC ERROR HANDLING.
+export const asyncErrorHandler = (fn: (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+) => Promise<any>) => {
+    // Returning a new function which runs "fn", catches it's error and passes it to the global error middleware i.e."errorHandler".
+    return (
+        req: Request,
+        res: Response,
+        next: NextFunction,
+    ): Promise<any> => fn(
+        req,
+        res,
+        next,
+    ).catch(next); // catch(next) catches the error and calls next(error) which in turn passes the error to "errorHandler" i.e. our global error-handling middleware.
+};
